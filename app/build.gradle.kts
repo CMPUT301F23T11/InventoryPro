3a8e967670297b03fab645fb37b98b74f2057a47
chasejohnson626 <79180631+chasejohnson626@users.noreply.github.com>
2023-10-26 13:52:28 -0600

    changed sdk version to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.inventorypro"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.inventorypro"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
