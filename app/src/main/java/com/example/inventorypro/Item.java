9addf08a5bd34d272a5923f75786370f6f5f259c
chasejohnson626 <79180631+chasejohnson626@users.noreply.github.com>
2023-10-26 14:43:37 -0600

    added Item class

---
+++ app/src/main/java/com/example/inventorypro/Item.java
@@ -0,0 +1,94 @@
+package com.example.inventorypro;
+
+import java.util.Date;
+
+public class Item {
+    private String name;
+    private Float value;
+    private Date date;
+    private String make;
+
+    // This is String type so it can be any length, and sometimes serial numbers
+    // contain characters.
+    private String serialNumber;
+    private String description;
+    private String comment;
+
+    // TODO - images
+    // TODO - tags
+    // TODO - database management
+
+    /**
+     * Constructor
+     * @param name the name of the item
+     * @param value the value of the item
+     * @param date the date of purchase or acquisition of the item
+     * @param make the make of the item
+     * @param serialNumber the serial number of the item
+     * @param description the description of the item
+     * @param comment a comment for the item
+     */
+    public Item(String name,
+                Float value,
+                Date date,
+                String make,
+                String serialNumber,
+                String description,
+                String comment) {
+        this.name = name;
+        this.value = value;
+        this.make = make;
+        this.serialNumber = serialNumber;
+        this.description = description;
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getValue() {
+        return value;
+    }
+    public void setValue(Float value) {
+        this.value = value;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getMake() {
+        return make;
+    }
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
