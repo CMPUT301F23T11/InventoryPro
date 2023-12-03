cd8578f3c81b3a7330909f4045914fcc4c0bd56e
Sahir Momin <sahirmomin65@gmail.com>
2023-12-02 23:32:06 -0700

    images displaying in edit item too

--- app/src/main/java/com/example/inventorypro/Activities/AddItemActivity.java
+++ app/src/main/java/com/example/inventorypro/Activities/AddItemActivity.java
@@ -1,5 +1,6 @@
 package com.example.inventorypro.Activities;
 
+import static androidx.core.content.ContentProviderCompat.requireContext;
 import static java.lang.Integer.parseInt;
 
 import android.app.AlertDialog;
@@ -31,6 +32,7 @@ import androidx.viewpager2.widget.CompositePageTransformer;
 import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.inventorypro.DatabaseManager;
 import com.example.inventorypro.Fragments.SelectTagsFragment;
 import com.example.inventorypro.Helpers;
 import com.example.inventorypro.Item;
@@ -42,6 +44,7 @@ import com.google.android.gms.common.api.OptionalModuleApi;
 import com.google.android.gms.common.moduleinstall.ModuleInstall;
 import com.google.android.gms.common.moduleinstall.ModuleInstallClient;
 import com.google.android.gms.common.moduleinstall.ModuleInstallRequest;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning;
 
@@ -238,39 +241,44 @@ public class AddItemActivity extends AppCompatActivity {
             description.getEditText().setText(potentialItem.getDescription());
             comments.getEditText().setText(potentialItem.getComment());
             tags.addAll(potentialItem.getTags());
-            uid = potentialItem.getUID();
+            if (potentialItem.getImageUris().size() > 0) {
+                DatabaseManager.getImageUris(this, potentialItem.getImageUris(), new OnSuccessListener<List<Uri>>() {
+                    @Override
+                    public void onSuccess(List<Uri> uris) {
+                        int count = potentialItem.getImageUris().size();
+                        for (int i = 0; i < count; i++) {
+                            sliderItems.add(new SliderItem(uris.get(i)));
+                        }
+                        SliderAdapter sliderAdapter = new SliderAdapter(sliderItems, viewPager2);
 
-            //TODO: load in images
-            ArrayList<Uri> imageUris = new ArrayList<>();
-            for (String s : potentialItem.getImageUris()){
-                Uri i = Uri.parse(s);
-                if (s==null)continue;
-                imageUris.add(i);
-            }
-            for (int i = 0; i < imageUris.size(); i++) {
-                Uri uri = imageUris.get(i);
-                sliderItems.add(new SliderItem(uri));
-            }
-            viewPager2.setAdapter(new SliderAdapter(sliderItems,viewPager2));
-            viewPager2.setBackground(null);
-            viewPager2.setClipToPadding(false);
-            viewPager2.setClipChildren(false);
-            viewPager2.setOffscreenPageLimit(3);
-            viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
-            CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
-            compositePageTransformer.addTransformer(new MarginPageTransformer(5));
-            compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
-                @Override
-                public void transformPage(@NonNull View page, float position) {
-                    float r = 1 - Math.abs(position);
-                    page.setScaleY(0.85f + r * 0.15f);
-                }
-            });
-            viewPager2.setPageTransformer(compositePageTransformer);
+                        viewPager2.setAdapter(sliderAdapter);
+                        // Save the URI to the global variable if needed for later use
+                        // this.uri = uri;
+                        sliderAdapter.notifyDataSetChanged();
+                        viewPager2.setBackground(null);
 
-            // Change the header to "Edit Item"
-            header.setText("Edit Item");
-            editMode = true;
+                        viewPager2.setClipToPadding(false);
+                        viewPager2.setClipChildren(false);
+                        viewPager2.setOffscreenPageLimit(3);
+                        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+
+                        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+                        compositePageTransformer.addTransformer(new MarginPageTransformer(5));
+                        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+                            @Override
+                            public void transformPage(@NonNull View page, float position) {
+                                float r = 1 - Math.abs(position);
+                                page.setScaleY(0.85f + r * 0.15f);
+                            }
+                        });
+
+                        viewPager2.setPageTransformer(compositePageTransformer);
+
+
+                    }
+                });
+            }
         } else if (potentialSerialNumber != null) {
             serialNumber.getEditText().setText(potentialSerialNumber);
         }
